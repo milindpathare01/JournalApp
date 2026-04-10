70d203d9f39e4d2a0f8b8f2e022bd01c1fc26cde
milind <milindpathare2001@gmail.com>
2026-04-10 20:50:32 +0530

    deleting test

--- src/test/java/com/ex_mongo_2/demo_2/Demo2ApplicationTests.java
+++ src/test/java/com/ex_mongo_2/demo_2/Demo2ApplicationTests.java
@@ -1,11 +1,13 @@
 package com.ex_mongo_2.demo_2;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Demo2ApplicationTests {
-
+	
+	@Disabled
 	@Test
 	void contextLoads() {
 	}
